d24a932a7a44f93c32f49c858ba7b43511f6bd09
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-28 20:19:19 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -12,9 +12,36 @@ document.addEventListener("DOMContentLoaded", ready());
   document.getElementById('minus').addEventListener("click",(e)=>{
     i = i-1;
     })
+    document.getElementById('plus').addEventListener("click",(e)=>{
+        i = i+1;
+    })
     document.getElementById('heart').addEventListener("click",(e)=>{
 
         document.querySelector('.likes').innerHTML  +=`
         <li>Liked at ${i}</li>
         `
-  
+    })
+    document.getElementById('pause').addEventListener("click",(e)=>{
+        if(paused){
+            ready();
+
+        document.getElementById('pause').innerText = "pause";
+        }
+        else{
+        clearInterval(set);
+        document.getElementById('pause').innerText = "resume";
+        paused  = true;
+    }})
+    let comments = 0
+    document.getElementById("comment-form").addEventListener("submit",(e)=>{
+        e.preventDefault();
+        let text  = document.getElementById("comment-input").value;
+        comments++;
+        document.getElementById("list").innerHTML+= `
+        <h5>Comment ${comments}:</h5>
+        <p>${text}<p>`
+
+    })
+
+
+}
